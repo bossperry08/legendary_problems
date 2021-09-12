51d257090dc0cf1ec5e4866548d8906f74cb7f61
Boss Perry (Pez) <88496461+bossperry08@users.noreply.github.com>
2021-09-12 18:58:42 +0700

    Update chicken_and_dog.py
    Python shorter version of "chicken and dog" Maths problem :)))

--- chicken_and_dog.py
+++ chicken_and_dog.py
@@ -1,19 +1,12 @@
 numLeg1d = 4
 numLeg1c = 2
-dog_img = dog = chick = 0
-while dog_img <= 36:
-    chick_img = 0
-    while chick_img <= 36:
-        sum_leg = dog_img * numLeg1d + chick_img * numLeg1c
-        sum_dc = dog_img + chick_img
-        if sum_leg == 100 and sum_dc == 36:
-            dog, chick = dog_img, chick_img
-            print("Chickens:", chick)
-            print("Dogs:", dog)
-            break
-        chick_img += 1
-        if dog != 0 and chick != 0:
-            break
-    dog_img += 1
+dog, chick = 36, 0
+while True:
+    if (dog + chick == 36) and (dog * numLeg1d + chick * numLeg1c == 100):
+        break;
+    dog -= 1
+    chick +=1
+print("So ga:", chick)
+print("So cho:", dog)
         
         
