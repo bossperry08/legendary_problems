4c633fb8dca280e617992ed3ecccf7927714ee5e
Boss Perry (Pez) <88496461+bossperry08@users.noreply.github.com>
2021-09-12 19:04:19 +0700

    Update chicken_and_dog.cpp
    C++ shorter version of "chicken and dog" Maths problem.

--- chicken_and_dog.cpp
+++ chicken_and_dog.cpp
@@ -15,28 +15,17 @@ int main()
     cout << "Yeu cau: Tinh so ga va so cho." << '\n';
     cout << "Phuong phap giai: Su dung 2 vong lap for long nhau, gia su tam." << '\n';
     cout << "Dap an: " << endl;
-    int dog = 0, chick = 0;
+    int dog = 36, chick = 0;
     //Can cong cu phu hop cho phuong phap gia thiet tam
-    for(int dog_img = 0; dog_img <= 36; dog_img++)
+    for(;;dog--, chick++)
     {
-        for(int chick_img = 0; chick_img <= 36; chick_img++)
-        {
-            int sum_dc = chick_img + dog_img;
-            int sum_leg = chick_img * numLeg1c + dog_img * numLeg1d;
-            if((sum_dc == 36) && (sum_leg == 100))
-            {
-                chick = chick_img;
-                dog = dog_img;
-                cout << "So ga: " << chick <<  " con" << endl;
-                cout << "So cho: " << dog <<  " con" << endl;
-                break;
-            }
-        }
-        if((dog != 0) && (chick != 0))
+        if((dog + chick == 36) && ((dog * numLeg1d) + (chick * numLeg1c) == 100))
         {
             break;
         }
     }
+    cout << "So ga: " << chick << endl;
+    cout << "So cho: " << dog << endl;
     cout << "Cam on da su dung chuong trinh." << endl;
 
     return 0;
